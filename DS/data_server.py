8cdcc7b38813bd45f8da39c0197173afa97d2b02
hacker <hacker@st10.os3.site>
2017-11-26 15:24:31 +0300

    Restructured project

--- DS/data_server.py
+++ DS/data_server.py
@@ -6,14 +6,12 @@ from rpyc.utils.server import ThreadedServer
 
 DATA_DIR = "files/"
 
-
+def request_file_structure():
+    conn = rpyc.connect('localhost', 2131)
 
 
 class DataService(rpyc.Service):
     class exposed_DataServer:
-
-        blocks = {}
-
         def exposed_put(self, file_path, data, minions):
             with open(DATA_DIR + str(file_path), 'wb') as f:
                 f.write(data)
@@ -45,8 +43,6 @@ class DataService(rpyc.Service):
         def exposed_delete_file(self, file_path):
             os.remove(file_path)
 
-        def exposed_ping(self):
-            return True
 
 if __name__ == "__main__":
     if not os.path.isdir(DATA_DIR):
